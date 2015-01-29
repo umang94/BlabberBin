b9a4858b81822ce9b6da7d55abc64c59a61841f4
Umang Jain <umang94@gmail.com>
2015-01-29 17:53:26 +0530

    Added authorisation files with facebook app id and app secret

---
+++ config/auth.js
@@ -0,0 +1,9 @@
+// Exposes the configuration setting for oAuth
+
+module.exports = {
+	'facebookAuth' : {
+		'clientID' : '774056715980815',
+		'clientSecret' : '514d75419086986801a13708436912ea',
+		'callbackURL' : 'http://localhost:8080/auth/facebook/callback'
+	}
+};
